4fd958905e0395125c87353ed61f8a55c270cf1f
wangxiaoyu.wang <wangxiaoyu.wang@qunar.com>
2016-03-07 14:45:31 +0800

    change css style and simplify the js code

--- js/app.js
+++ js/app.js
@@ -1,87 +1,89 @@
 'use strict';
 var wm = new Vue({
-  el: "#markdown",
-  data: {
-    sourceContent: "",
-    fullScreetEditState: "全屏编辑",
-    fullScreenPreviewState: "预览",
-    isSaved:false
-  },
-  methods: {
-    dropHandler: function(event) {
-      var that = this;
-      var files = event.dataTransfer.files;
-      for (var i = 0, len = files.length; i < len; i++) {
-        var reader = new FileReader();
-        if (/text/.test(files[i].type) || /\.md$/i.test(files[i].name)) {
-          reader.readAsText(files[i]);
-          reader.onload = function() {
-            that.sourceContent = reader.result;
-          }
+    el: "#markdown",
+    data: {
+        sourceContent: "",
+        isFullScreenPreview: false,
+        isFullScreenEdit:false,
+        isSaved: false
+    },
+    methods: {
+        dropHandler: function (event) {
+            var that = this;
+            var files = event.dataTransfer.files;
+            for (var i = 0, len = files.length; i < len; i++) {
+                var reader = new FileReader();
+                if (/text/.test(files[i].type) || /\.md$/i.test(files[i].name)) {
+                    reader.readAsText(files[i]);
+                    reader.onload = function () {
+                        that.sourceContent = reader.result;
+                    }
+                } else {
+                    alert('请拖放文本文件，谢谢！');
+                }
+            }
+        },
+        toggleFullScreenEdit: function () {
+            this.isFullScreenEdit = !this.isFullScreenEdit;
+            if(this.isFullScreenEdit){
+                this.$el.classList.add('fullscreen-edit');
+            }else{
+                this.$el.classList.remove('fullscreen-edit');
+            }
+        },
+        toggleFullScreenPreview: function () {
+            this.isFullScreenPreview = !this.isFullScreenPreview;
+            if(this.isFullScreenPreview){
+                this.$el.classList.add('fullscreen-preview');
+            }else{
+                this.$el.classList.remove('fullscreen-preview');
+            }
+        },
+        save: function () {
+            if (this.isSaved == false) {
+                window.localStorage.setItem('markdown-text', this.sourceContent);
+                this.isSaved = true;
+            }
+        },
+        print: function () {
+            window.print();
+        }
+    },
+    ready: function () {
+        var text = window.localStorage.getItem('markdown-text');
+        if (text) {
+            // because this Vue instance is already in DOM
+            // so ready event will be fired before Vue instance 
+            // create complete,so watch will not notice this change
+            // becase vm.$watch is added after ready hook fired
+            // so use nextTick let this change noticed by $watch.
+            this.$nextTick(function () {
+                this.sourceContent = text;
+            });
         } else {
-          alert('请拖放文本文件，谢谢！');
+            var xmlhttp;
+            if (window.XMLHttpRequest) {
+                xmlhttp = new XMLHttpRequest();
+            } else {
+                xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+            }
+            xmlhttp.open('GET', 'src/example.md');
+            xmlhttp.send();
+            xmlhttp.onreadystatechange = function () {
+                if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                    this.sourceContent = xmlhttp.responseText;
+                }
+            };
         }
-      }
-    },
-    toggleFullScreenEdit: function() {
-      this.fullScreetEditState = this.fullScreetEditState == "全屏编辑" ? "退出全屏" : "全屏编辑";
-      if (this.$el.classList.contains('fullscreen-edit')) {
-        this.$el.classList.remove('fullscreen-edit');
-      } else {
-        this.$el.classList.remove('fullscreen-preview');
-        this.$el.classList.add('fullscreen-edit');
-      }
-    },
-    toggleFullScreenPreview: function() {
-      this.fullScreenPreviewState = this.fullScreenPreviewState == "预览" ? "退出预览" : "预览";
-      if (this.$el.classList.contains('fullscreen-preview')) {
-        this.$el.classList.remove('fullscreen-preview');
-      } else {
-        this.$el.classList.remove('fullscreen-edit');
-        this.$el.classList.add('fullscreen-preview');
-      }
-    },
-    save:function(){
-    		if(this.isSaved == false){
-        	window.localStorage.setItem('markdown-text',this.sourceContent);
-    			this.isSaved = true;
-    		}
+        // save content every 5s
+        setInterval(this.save, 50000);
+        window.addEventListener('unload', this.save, false);
     }
-  },
-  ready: function() {
-    var text = window.localStorage.getItem('markdown-text');
-    if (text) {
-    	// because this Vue instance is already in DOM
-    	// so ready event will be fired before Vue instance 
-    	// create complete,so watch will not notice this change
-    	// becase vm.$watch is added after ready hook fired
-    	// so use nextTick let this change noticed by $watch.
-    	this.$nextTick(function () {
-      	this.sourceContent = text;
-    	});
-    } else {
-      var xmlhttp;
-      if (window.XMLHttpRequest) {
-        xmlhttp = new XMLHttpRequest();
-      } else {
-        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-      }
-      xmlhttp.open('GET', 'src/example.md');
-      xmlhttp.send();
-      xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-          this.sourceContent = xmlhttp.responseText;
-        }
-      };
-    }
-    // save content every 5s
-    setInterval(this.save, 50000);
-    window.addEventListener('unload', this.save, false);
-  }
 });
 
-wm.$watch("sourceContent", function(text) {
-  var dist = document.getElementById('dist');
-  dist.innerHTML = marked(text);
-  this.isSaved = false;
+wm.$watch("sourceContent", function (text) {
+    // if(this.fullScreetEditState == )
+    var dist = document.getElementById('dist');
+    dist.innerHTML = marked(text);
+    this.isSaved = false;
 });
