8618858296090a096912570c905ce18a718a8df5
wy <nameiswangyu@163.com>
2018-03-04 16:06:03 +0800

    delete useless file

--- monaco-editor-worker-loader-proxy.js
+++
@@ -1,5 +0,0 @@
-self.MonacoEnvironment = {
-    baseUrl: 'https://cdn.bootcss.com/monaco-editor/0.10.1/min'
-};
-importScripts('https://cdn.bootcss.com/require.js/2.3.5/require.min.js');
-importScripts('https://cdn.bootcss.com/monaco-editor/0.10.1/min/vs/language/typescript/src/worker.js');
